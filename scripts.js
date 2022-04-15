0e594ff49d2126620a3323e281671166e1e51975
CaioVitor1 <caiovitor33@hotmail.com>
2022-04-15 00:21:04 -0300

    Buscando mensagens da API

--- scripts.js
+++ scripts.js
@@ -54,6 +54,58 @@ function quandoErroConexao() {
     console.log("Não estamos mantendo você conectado")
 }
 
+// Buscando mensagens da API
+function buscandomensagens() {
+    const promise = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages");
+
+    promise.then(carregandoMensagens);
+    promise.catch(erroMensagens)
+}
+
+function carregandoMensagens(mensagens) {
+    console.log("As mensagens foram carregadas")
+    pastaMensagens = mensagens.data 
+    console.log("A quantidade de mensagens é: " + pastaMensagens.length)
+    renderizandoMensagens();
+}
+
+function renderizandoMensagens() {
+    let todasAsMensagens = document.querySelector(".conteudo-principal");
+     todasAsMensagens.innerHTML = ""
+     for (let i=0; i < pastaMensagens.length; i++) {
+        if (pastaMensagens[i].type == "status") {
+            todasAsMensagens.innerHTML += `<div class="entrar-na-sala">
+           <h3> (${pastaMensagens[i].time}) ${pastaMensagens[i].from}  ${pastaMensagens[i].text} </h3>
+     </div>
+         `
+        } else if (pastaMensagens[i].type == "message") {
+            todasAsMensagens.innerHTML += `<div class="mensagem-todos">
+            <h3> (${pastaMensagens[i].time}) ${pastaMensagens[i].from}  ${pastaMensagens[i].text} </h3>
+     </div>
+         `
+        }    
+        else if (pastaMensagens[i].type == "private_message") {
+            todasAsMensagens.innerHTML += `<div class="mensagem-reservada">
+            <h3> (${pastaMensagens[i].time}) ${pastaMensagens[i].from}  ${pastaMensagens[i].text} </h3>
+     </div>
+         `
+        }   
+         
+
+     }   
+
+
+}
+
+
+
+function erroMensagens() {
+    console.log("As mensagens não foram carregadas")
+}
+
+
+
+
 
 
 pegarNome()
